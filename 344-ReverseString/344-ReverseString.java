5ad40cf106fa1ba5971f7862097fdf889c575b14
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2025-08-12 09:51:42 +0530

    File updated at 8/12/2025, 9:51:38 AM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,15 +1,13 @@
-// Last updated: 8/12/2025, 9:51:07 AM
+// Last updated: 8/12/2025, 9:51:38 AM
 class Solution {
     public void reverseString(char[] s) {
        int left = 0;
        int right = s.length-1;
        while(left < right){
         char temp=s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        left++;
-        right--;
+        s[left++] = s[right];
+        s[right--] = temp;
        } 
-       
+
     }
 }
