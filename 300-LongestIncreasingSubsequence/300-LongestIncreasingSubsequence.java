cd3500d53127ed9f05d9e2874061c6b1765e6234
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2025-11-04 09:37:55 +0530

    File updated at 11/4/2025, 9:37:54 AM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,4 +1,17 @@
-// Last updated: 11/4/2025, 9:37:06 AM
+// Last updated: 11/4/2025, 9:37:54 AM
+// class Solution {
+//     public static int solve(int[] nums, int idx, int prev){
+//         if(idx == nums,length) return 0;
+//         int include = 0;
+//         int exclude = 0;
+//         if(prev == -1 || nums)
+//     }
+//     public int lengthOfLIS(int[] nums) {
+//         int prev = -1;
+//         return solve(nums,0,prev);
+//     }
+// }
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if(nums.length == 0) return 0;
