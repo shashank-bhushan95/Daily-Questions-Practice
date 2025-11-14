2add53df9a8d89fd4c9e260083ad1b3bf56c60b0
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2025-11-14 10:34:53 +0530

    File updated at 11/14/2025, 10:34:52 AM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,17 +1,39 @@
-// Last updated: 11/11/2025, 9:37:36 AM
+// Last updated: 11/14/2025, 10:34:52 AM
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] arr = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0 || j == 0){
-                    arr[i][j] = 1;
+    public int uniquePathsWithObstacles(int[][] og) {
+        if(og[0][0] == 1) return 0;
+        og[0][0] = 1;
+        int m = og.length;
+        int n = og[0].length;
+        for(int i = 1; i < m; i++){
+            if(og[i][0] == 1){
+                og[i][0] = 0;
+            }
+            else{
+                og[i][0] = og[i-1][0];
+            }
+        }
+
+        for(int j = 1; j < n; j++){
+            if(og[0][j] == 1){
+                og[0][j] = 0;
+            }
+            else{
+                og[0][j] = og[0][j-1];
+            }
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(og[i][j] == 1){
+                    og[i][j] = 0;
                 }
                 else{
-                    arr[i][j] = arr[i-1][j] + arr[i][j-1];
+                    og[i][j] = og[i-1][j] + og[i][j-1];
                 }
             }
         }
-        return arr[m-1][n-1];
-     }
+        return og[m-1][n-1];
+    }
+    
 }
