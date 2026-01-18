5449806f157a8843e1ab1dcabc517834a0dc62f6
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-01-18 15:23:19 +0530

    File updated at 1/18/2026, 3:23:17 PM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,13 +1,24 @@
-// Last updated: 8/12/2025, 9:51:38 AM
-class Solution {
-    public void reverseString(char[] s) {
-       int left = 0;
-       int right = s.length-1;
-       while(left < right){
-        char temp=s[left];
-        s[left++] = s[right];
-        s[right--] = temp;
-       } 
-
-    }
-}
+// Last updated: 1/18/2026, 3:23:17 PM
+1class Solution {
+2    public boolean isPalindrome(String s) {
+3        int i = 0;
+4        int j = s.length()-1;
+5
+6        while(i < j){
+7            if(!Character.isLetterOrDigit(s.charAt(i))){
+8                i = i+1;
+9                continue;
+10            }
+11            if(!Character.isLetterOrDigit(s.charAt(j))){
+12                j = j-1;
+13                continue;
+14            }
+15            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+16                return false;
+17            }
+18            i = i+1;
+19            j = j-1;
+20        }
+21        return true;
+22    }
+23}
