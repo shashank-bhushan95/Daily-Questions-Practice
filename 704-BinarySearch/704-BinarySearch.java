69e244b6f32bd49ea6deed77ed1fe6367612c6ff
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-01-21 16:04:25 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 1/21/2026, 4:04:25 PM
+1class Solution {
+2    public static int logic(int[] nums, int target, int start, int end){
+3        if(start > end) return -1;
+4        int mid = (start+end)/2;
+5        if(nums[mid] == target) return mid;
+6        else if(nums[mid] < target){
+7            return logic(nums,target,mid+1,end);
+8        }
+9        else{
+10            return logic(nums,target,start,mid-1);
+11        }
+12    }
+13    public int search(int[] nums, int target) {
+14        int start = 0;
+15        int end = nums.length-1;
+16        
+17        return logic(nums,target,start,end);
+18    }
+19}
