5380857a972cf246fb53ece872c925c15a40f0a4
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-04-02 18:00:10 +0530

    File updated at 4/2/2026, 6:00:05 PM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,21 +1,33 @@
-// Last updated: 1/29/2026, 10:39:21 PM
+// Last updated: 4/2/2026, 6:00:05 PM
 1class Solution {
-2    public int countPairs(List<Integer> nums, int target) {
-3        Collections.sort(nums);
-4        int start = 0;
-5        int end = nums.size()-1;
-6        int count = 0;
-7
-8        while(start < end){
-9            int sum = nums.get(start) + nums.get(end);
-10            if(sum < target){
-11                count = count + (end - start);
-12                start++;
-13            }
-14            else{
-15                end--;
-16            }
-17        }
-18        return count;
-19    }
-20}
+2    public void merge(int[] nums1, int m, int[] nums2, int n) {
+3        int i = m-1;
+4        int j = n-1;
+5        int index = nums1.length-1;
+6
+7        while(i >= 0 && j >= 0){
+8            if(nums1[i] > nums2[j]){
+9                nums1[index] = nums1[i];
+10                i--;
+11                index--;
+12            }
+13            else{
+14                nums1[index] = nums2[j];
+15                j--;
+16                index--;
+17            }
+18        }
+19
+20        while(i >= 0){
+21            nums1[index] = nums1[i];
+22            i--;
+23            index--;
+24        }
+25
+26        while(j >= 0){
+27            nums1[index] = nums2[j];
+28            j--;
+29            index--;
+30        }
+31    }
+32}
