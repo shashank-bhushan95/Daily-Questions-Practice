f4df1d55cb39e0c2fe4b21ba38d4a8df3437b340
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-04-09 21:53:05 +0530

    File updated at 4/9/2026, 9:53:01 PM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,14 +1,24 @@
-// Last updated: 4/9/2026, 9:49:47 PM
+// Last updated: 4/9/2026, 9:53:01 PM
 1class Solution {
 2    public void reverseString(char[] s) {
-3        char[] help = new char[s.length];
-4        int index = 0;
+3        // char[] help = new char[s.length];
+4        // int index = 0;
 5
-6        for(int i = 0; i < s.length; i++) help[i] = s[i];
+6        // for(int i = 0; i < s.length; i++) help[i] = s[i];
 7
-8        for(int i = help.length-1; i >= 0; i--) {
-9            s[index++] = help[i];
-10        }
-11        //return s;
-12    }
-13}
+8        // for(int i = help.length-1; i >= 0; i--) {
+9        //     s[index++] = help[i];
+10        // }
+11
+12
+13        int i = 0; 
+14        int j = s.length-1;
+15        while(i < j){
+16            char temp = s[i];
+17            s[i] = s[j];
+18            s[j] = temp;
+19            i++;
+20            j--;
+21        }
+22    }
+23}
