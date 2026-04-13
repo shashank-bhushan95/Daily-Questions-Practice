a13f4763b046e9f4fd0c3fbc67557e062ee27eb5
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-04-13 14:39:30 +0530

    File updated at 4/13/2026, 2:39:30 PM

--- 680-ValidPalindromeII/680-ValidPalindromeII.java
+++ 680-ValidPalindromeII/680-ValidPalindromeII.java
@@ -1,27 +1,26 @@
-// Last updated: 1/27/2026, 10:27:48 PM
+// Last updated: 4/13/2026, 2:39:30 PM
 1class Solution {
-2    public boolean helper(int i, int j, String s){
-3        while(i < j){
-4            if(s.charAt(i) != s.charAt(j)){
-5                return false;
-6            }
-7            i=i+1;
-8            j=j-1;
-9        }
-10        return true;
-11    }
-12    public boolean validPalindrome(String s) {
-13        int i = 0;
-14        int j = s.length()-1;
+2    public boolean helper(String s, int i, int j){
+3        while(i <= j){
+4            if(s.charAt(i) != s.charAt(j)) return false;
+5            i++;
+6            j--;
+7        }
+8        return true;
+9    }
+10
+11    public boolean validPalindrome(String s) {
+12        int i = 0;
+13        int j = s.length()-1;
+14
 15        while(i < j){
 16            if(s.charAt(i) != s.charAt(j)){
-17                return helper(i+1, j, s) || helper(i, j-1, s);
+17                return helper(s, i+1, j) || helper(s, i, j-1);
 18            }
-19            else{
-20                i=i+1;
-21                j=j-1;
-22            }
-23        }
-24        return true;
-25    }
-26}
+19            i++;
+20            j--;
+21        }
+22
+23        return true;
+24    }
+25}
