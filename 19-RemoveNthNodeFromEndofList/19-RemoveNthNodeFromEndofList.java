84f8e777a815ade390569119830ca2befb12010a
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-04-15 22:26:07 +0530

    File updated at 4/15/2026, 10:26:05 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,4 +1,4 @@
-// Last updated: 4/14/2026, 5:41:59 PM
+// Last updated: 4/15/2026, 10:26:05 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -11,44 +11,24 @@
 10 */
 11class Solution {
 12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13    //     int len = 0;
-14    //     ListNode temp = head;
-15        
-16    //     while(temp != null){
-17    //         len++;
-18    //         temp = temp.next;
-19    //     }
+13        ListNode dummy = new ListNode();
+14        dummy.next = head;
+15        int len = 0;
+16        while(dummy.next != null){
+17            len++;
+18            dummy = dummy.next;
+19        }
 20
-21    //     if (len == n) return head.next;
-22
-23    //     int diff = len - n;
-24    //     temp = head;
-25    //     for (int i = 1; i < diff; i++) {
-26    //       temp = temp.next;
-27    //     }
-28    //     temp.next = temp.next.next;
-29    //     return head;
-30
-31
-32    ListNode dummy = new ListNode(0);
-33    dummy.next = head;
-34
-35    ListNode after = dummy;
-36    ListNode curr = dummy;
-37
-38    for(int i = 0; i < n; i++) {
-39        after = after.next;
-40    }
-41
-42    while(after.next != null) {
-43        after = after.next;
-44        curr = curr.next;
-45    }
-46
-47    curr.next = curr.next.next;
-48
-49    return dummy.next;
-50
-51
-52    }
-53}
+21        if(len == n) return head.next;
+22        int diff = len-n;
+23
+24        ListNode temp = new ListNode();
+25        temp.next = head;
+26        for(int i = 0; i < diff; i++){
+27            temp = temp.next;
+28        }
+29
+30        temp.next = temp.next.next;
+31        return head;
+32    }
+33}
