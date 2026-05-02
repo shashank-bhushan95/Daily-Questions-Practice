86c36e3f1eb31f713ffb1a59350570dbb3d459be
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-05-02 22:39:16 +0530

    File updated at 5/2/2026, 10:39:12 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,4 +1,4 @@
-// Last updated: 5/2/2026, 10:39:04 PM
+// Last updated: 5/2/2026, 10:39:12 PM
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
 3        // // Using Nested Loop
