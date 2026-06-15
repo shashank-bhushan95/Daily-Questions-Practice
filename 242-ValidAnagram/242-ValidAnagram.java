0762b618a35773e9a7194fd0d367c7b51b5d78c1
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-15 12:49:04 +0530

    File updated at 6/15/2026, 12:49:03 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,21 +1,12 @@
-// Last updated: 6/15/2026, 12:39:37 PM
+// Last updated: 6/15/2026, 12:49:03 PM
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3        if(s.length() != t.length()){
-4            return false;
-5        }
-6        
-7        char cs[] = s.toCharArray();
-8        char ct[] = t.toCharArray();
-9        Arrays.sort(cs);
-10        Arrays.sort(ct);
-11
-12        for(int i = 0; i < cs.length; i++){
-13            if(cs[i] != ct[i]){
-14                return false;
-15            }
-16        }
-17        return true;
-18    }
-19}
-20
+3        char[] a = s.toCharArray();
+4        char[] b = t.toCharArray();
+5
+6        Arrays.sort(a);
+7        Arrays.sort(b);
+8
+9        return Arrays.equals(a,b);
+10    }
+11}
