ebf943197d862bb5369107ee1b1343e20a9c9d99
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-16 10:15:51 +0530

    File updated at 6/16/2026, 10:15:49 AM

--- 412-FizzBuzz/412-FizzBuzz.java
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -1,15 +1,17 @@
-// Last updated: 6/16/2026, 10:10:56 AM
+// Last updated: 6/16/2026, 10:15:49 AM
 1class Solution {
-2    public int[] plusOne(int[] digits) {
-3        for(int i = digits.length-1; i >= 0; i--){
-4            if(digits[i] < 9){
-5                digits[i]++;
-6                return digits;
-7            }
-8            digits[i]  = 0;
-9        }
-10        int[] arr = new int[digits.length+1];
-11        arr[0] = 1;
-12        return arr;
-13    }
-14}
+2    public int numberOfSteps(int num) {
+3        int count = 0;
+4        while(num > 0){
+5            if(num % 2 == 0){
+6                count++;
+7                num = num / 2;
+8            }
+9            else{
+10                num = num - 1;
+11                count++;
+12            }
+13        }
+14        return count;
+15    }
+16}
