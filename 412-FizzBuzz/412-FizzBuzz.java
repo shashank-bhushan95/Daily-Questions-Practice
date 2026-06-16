98a1a26974ac28c1dcd5b8809eada24ab2b5fe77
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-16 09:43:03 +0530

    File updated at 6/16/2026, 9:43:01 AM

--- 412-FizzBuzz/412-FizzBuzz.java
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -1,21 +1,15 @@
-// Last updated: 6/16/2026, 9:19:49 AM
+// Last updated: 6/16/2026, 9:43:01 AM
 1class Solution {
-2    public List<String> fizzBuzz(int n) {
-3        List<String> ans = new ArrayList<>();
-4        for(int i = 1; i <= n; i++){
-5            if(i % 3 == 0 && i % 5 == 0){
-6                ans.add("FizzBuzz");
-7            }
-8            else if(i % 3 == 0){
-9                ans.add("Fizz");
-10            }
-11            else if(i % 5 == 0){
-12                ans.add("Buzz");
-13            }
-14            else{
-15                ans.add(i+"");
-16            }
-17        }
-18        return ans;
-19    }
-20}
+2    public int reverse(int x) {
+3        long rev = 0;
+4        // int copy = x;
+5        while(x != 0){
+6            int digit = x % 10;
+7            x = x / 10;
+8            rev = rev * 10 + digit;
+9            if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) return 0;
+10        }
+11        
+12        return (int) rev;
+13    }
+14}
