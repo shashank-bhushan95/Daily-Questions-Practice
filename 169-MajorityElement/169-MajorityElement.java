c72f2e188d744726f23ef5eb9b1338ec87b4d031
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-18 00:24:09 +0530

    File updated at 6/18/2026, 12:23:02 AM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,21 +1,22 @@
-// Last updated: 6/18/2026, 12:14:23 AM
+// Last updated: 6/18/2026, 12:23:02 AM
 1class Solution {
-2    public int majorityElement(int[] nums) {
-3        int candidate = 0;
-4        int count = 0;
-5
-6        for (int i = 0; i < nums.length; i++) {
-7            if (count == 0) {
-8                candidate = nums[i];
-9            }
-10
-11            if (nums[i] == candidate) {
-12                count++;
-13            } else {
-14                count--;
-15            }
-16        }
-17
-18        return candidate;
-19    }
-20}
+2    public int findMaxConsecutiveOnes(int[] nums) {
+3        int curr = 0;
+4        int befo = -1;
+5        int ans = 0;
+6    
+7        for(int i = 0; i < nums.length; i++){
+8            
+9            if(nums[i] == 1){
+10                int one = curr - befo;
+11                if(one > ans) ans = one;
+12                curr++;
+13            }
+14            else{
+15                curr++;
+16                befo = i;
+17            }
+18        }
+19        return ans;
+20    }
+21}
