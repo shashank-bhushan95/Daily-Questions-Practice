99f58b755025e873c697f51ef7c0470ff3ca7c6c
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-17 10:34:44 +0530

    File updated at 6/17/2026, 10:33:37 AM

--- 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.java
+++ 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.java
@@ -1,18 +1,25 @@
-// Last updated: 6/17/2026, 10:18:22 AM
+// Last updated: 6/17/2026, 10:33:37 AM
 1class Solution {
 2    public int findNumbers(int[] nums) {
-3        
-4        int ans = 0;
-5        for(int i = 0; i < nums.length; i++){
-6            int count = 0;
-7            while(nums[i] > 0){
-8                nums[i] = nums[i] / 10;
-9                count++;
-10            }
-11            if(count % 2 == 0){
-12                ans++;
-13            }
-14        }
-15        return ans;
-16    }
-17}
+3        // N^2
+4        // int ans = 0;
+5        // for(int i = 0; i < nums.length; i++){
+6        //     int count = 0;
+7        //     while(nums[i] > 0){
+8        //         nums[i] = nums[i] / 10;
+9        //         count++;
+10        //     }
+11        //     if(count % 2 == 0){
+12        //         ans++;
+13        //     }
+14        // }
+15        // return ans;
+16
+17        int count = 0;
+18        for(int i = 0; i < nums.length; i++){
+19            int digit =(int) Math.log10(nums[i]) + 1;
+20            if(digit % 2 == 0) count++;
+21        }
+22        return count;
+23    }
+24}
