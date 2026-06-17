91374de21d1777ac56f0867cd65ee38dcb9848c7
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-18 00:47:13 +0530

    File updated at 6/18/2026, 12:46:06 AM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,36 +1,25 @@
-// Last updated: 6/18/2026, 12:40:19 AM
+// Last updated: 6/18/2026, 12:46:06 AM
 1class Solution {
-2    public int maxPower(String s) {
-3        // int curr = 0;
-4        // int before =-1;
-5        // int ans = 0;
-6        // for(int i = 1; i < s.length(); i++){
-7        //     if(s.charAt(i) == s.charAt(i-1)){
-8        //         curr++;
-9        //         int count = curr - before;
-10        //         if(count > ans){
-11        //             ans = count;
-12        //         }
-13        //     }
-14        //     else{
-15        //         curr++;
-16        //         before = i-1;
-17        //     }
-18        // } 
-19        // if(ans == 0) return 1;
-20        // return ans;
-21
-22        int ans = 1;
-23        int count = 1;
-24        for(int i = 1; i < s.length(); i++){
-25            if(s.charAt(i) == s.charAt(i-1)){
-26                count++;
-27                ans = Math.max(ans, count);
-28            }
-29            else{
-30                count = 1;
-31            }
-32        }
-33        return ans;
-34    }
-35}
+2    public void moveZeroes(int[] nums) {
+3        int count = 0;
+4        int[] ans = new int[nums.length];
+5        for(int i = 0; i < nums.length; i++){
+6            if(nums[i] == 0) count++;
+7        }
+8
+9        int index = 0;
+10        for(int i = 0; i < nums.length; i++){
+11            if(nums[i] != 0){
+12                ans[index++] = nums[i];
+13            }
+14        }
+15
+16        for(int i = 0; i < count; i++){
+17            ans[index++] = 0;
+18        }
+19
+20        for(int i = 0; i < nums.length; i++){
+21            nums[i] = ans[i];
+22        }
+23    }
+24}
