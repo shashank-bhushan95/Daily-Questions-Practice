6955916e46da62bd810b3154cb11df777f7c3179
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-18 10:42:07 +0530

    File updated at 6/18/2026, 10:40:59 AM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,14 +1,21 @@
-// Last updated: 6/18/2026, 9:47:11 AM
+// Last updated: 6/18/2026, 10:40:59 AM
 1class Solution {
-2    public int removeElement(int[] nums, int val) {
-3        int index = 0;
-4
-5        for(int i = 0; i < nums.length; i++){
-6            if(nums[i] == val) continue;
-7            else{
-8                nums[index++] = nums[i];
+2    public int[] applyOperations(int[] nums) {
+3        int[] ans = new int[nums.length];
+4        int index = 0;
+5        for(int i = 0; i < nums.length-1; i++){
+6            if(nums[i] == nums[i+1] && nums[i] != 0){
+7                nums[i] = nums[i] * 2;
+8                nums[i+1] = 0;
 9            }
 10        }
-11        return index;
-12    }
-13}
+11
+12        for(int i = 0; i < nums.length; i++){
+13            if(nums[i] != 0){
+14                ans[index++] = nums[i];
+15            }
+16        }
+17        
+18        return ans;
+19    }
+20}
