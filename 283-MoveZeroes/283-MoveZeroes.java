b4fb998a9836a4e86f9277ad2f0c5d3db714d09c
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-18 10:44:50 +0530

    File updated at 6/18/2026, 10:43:43 AM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,10 +1,10 @@
-// Last updated: 6/18/2026, 10:40:59 AM
+// Last updated: 6/18/2026, 10:43:43 AM
 1class Solution {
 2    public int[] applyOperations(int[] nums) {
 3        int[] ans = new int[nums.length];
 4        int index = 0;
 5        for(int i = 0; i < nums.length-1; i++){
-6            if(nums[i] == nums[i+1] && nums[i] != 0){
+6            if(nums[i] == nums[i+1]){
 7                nums[i] = nums[i] * 2;
 8                nums[i+1] = 0;
 9            }
