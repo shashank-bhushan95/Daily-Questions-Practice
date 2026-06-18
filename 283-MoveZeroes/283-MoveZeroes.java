25860793ca896e0b369848cecc38d7ce67ec25d8
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-18 09:10:11 +0530

    File updated at 6/18/2026, 9:09:03 AM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,18 +1,39 @@
-// Last updated: 8/7/2025, 10:03:40 PM
-class Solution {
-    public void moveZeroes(int[] nums) {
-        int index = 0;
-        int size = nums.length;
-        for(int i = 0; i < size; i++){
-            if(nums[i] != 0){
-                nums[index] = nums[i];
-                index++;
-            }
-        }
-        while(index < size){
-            nums[index] = 0;
-            index++;
-        }
-        System.out.println(nums);
-    }
-}
+// Last updated: 6/18/2026, 9:09:03 AM
+1class Solution {
+2    public void moveZeroes(int[] nums) {
+3        // int count = 0;
+4        // int[] ans = new int[nums.length];
+5        // for(int i = 0; i < nums.length; i++){
+6        //     if(nums[i] == 0) count++;
+7        // }
+8
+9        // int index = 0;
+10        // for(int i = 0; i < nums.length; i++){
+11        //     if(nums[i] != 0){
+12        //         ans[index++] = nums[i];
+13        //     }
+14        // }
+15
+16        // for(int i = 0; i < count; i++){
+17        //     ans[index++] = 0;
+18        // }
+19
+20        // for(int i = 0; i < nums.length; i++){
+21        //     nums[i] = ans[i];
+22        // }
+23
+24        int count = 0;
+25        int index = 0;
+26        for(int i = 0; i < nums.length; i++){
+27            if(nums[i] != 0){
+28                nums[index++] = nums[i];
+29            }
+30        }
+31
+32        count = nums.length - index;
+33
+34        for(int i = 0; i < count; i++){
+35            nums[index++] = 0;
+36        }
+37    }
+38}
