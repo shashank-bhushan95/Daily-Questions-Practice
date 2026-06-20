c5bf43f4208a43a5cf91e8f42b5065bf162cb2ff
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-20 17:13:19 +0530

    File updated at 6/20/2026, 5:13:17 PM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -1,30 +1,27 @@
-// Last updated: 6/20/2026, 4:32:27 PM
+// Last updated: 6/20/2026, 5:13:17 PM
 1class Solution {
 2    public int thirdMax(int[] nums) {
-3        Arrays.sort(nums);
-4        int[] arr = new int[nums.length];
-5        int index = 0;
-6        for(int i = nums.length-1; i >= 0; i--){
-7            arr[index++] = nums[i];
-8        }
-9        LinkedHashSet<Integer> h = new LinkedHashSet<>();// Because hashset to maintain indext but linked Hash set maintains index.
-10        for(int i = 0; i < arr.length; i++){
-11            h.add(arr[i]);
-12        }
-13
-14        int ans = 0;
-15        if(h.size() < 3){
-16            for(int num : h){
-17                return num;
-18            }
-19        }
-20        else{
-21            int index1 = 1;
-22            for(int num : h){
-23                if(index1 == 3) return num;
-24                index1++;
-25            }
-26        }
-27        return 0;
-28    }
-29}
+3        long m1 = Long.MIN_VALUE;
+4        long m2 = Long.MIN_VALUE;
+5        long m3 = Long.MIN_VALUE;
+6
+7        for(int i = 0; i < nums.length; i++){
+8            if(m1 == nums[i] || m2 == nums[i] || m3 == nums[i]) continue;
+9            if(nums[i] > m1){
+10                m3 = m2;
+11                m2 = m1;
+12                m1 = (long) nums[i];
+13            }
+14            else if(nums[i] > m2){
+15                m3 = m2;
+16                m2 = (long) nums[i];
+17            }
+18            else if(nums[i] > m3){
+19                m3 = (long) nums[i];
+20            }
+21        }
+22        if(m3 == Long.MIN_VALUE) return (int) m1;
+23        else return (int) m3;
+24        
+25    }
+26}
