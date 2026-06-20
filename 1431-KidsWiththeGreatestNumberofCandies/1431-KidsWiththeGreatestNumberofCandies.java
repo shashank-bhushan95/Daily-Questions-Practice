253d7ba99f671f2c80ff0003820391b7f1c60740
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-20 16:32:29 +0530

    File updated at 6/20/2026, 4:32:27 PM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -1,19 +1,30 @@
-// Last updated: 6/20/2026, 3:48:44 PM
+// Last updated: 6/20/2026, 4:32:27 PM
 1class Solution {
-2    public int missingNumber(int[] nums) {
-3        // Arrays.sort(nums);
-4        // for(int i = 0; i < nums.length; i++){
-5        //     if(nums[i] != i) return i;
-6        // }
-7        // return nums.length;
-8
-9        int n = nums.length;
-10        int sum = (n*(n+1))/2;
-11        int sumArray = 0;
-12        for(int i = 0; i < nums.length; i++){
-13            sumArray = sumArray + nums[i];
-14        }
-15        int ans = sum - sumArray;
-16        return ans;
-17    }
-18}
+2    public int thirdMax(int[] nums) {
+3        Arrays.sort(nums);
+4        int[] arr = new int[nums.length];
+5        int index = 0;
+6        for(int i = nums.length-1; i >= 0; i--){
+7            arr[index++] = nums[i];
+8        }
+9        LinkedHashSet<Integer> h = new LinkedHashSet<>();// Because hashset to maintain indext but linked Hash set maintains index.
+10        for(int i = 0; i < arr.length; i++){
+11            h.add(arr[i]);
+12        }
+13
+14        int ans = 0;
+15        if(h.size() < 3){
+16            for(int num : h){
+17                return num;
+18            }
+19        }
+20        else{
+21            int index1 = 1;
+22            for(int num : h){
+23                if(index1 == 3) return num;
+24                index1++;
+25            }
+26        }
+27        return 0;
+28    }
+29}
