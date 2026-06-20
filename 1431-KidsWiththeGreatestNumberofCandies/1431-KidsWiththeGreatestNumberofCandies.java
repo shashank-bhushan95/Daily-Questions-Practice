b3cbd7c961bd0f658d909e1b60bca31341ba366c
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-20 15:48:45 +0530

    Create 1431-KidsWiththeGreatestNumberofCandies

---
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,19 @@
+// Last updated: 6/20/2026, 3:48:44 PM
+1class Solution {
+2    public int missingNumber(int[] nums) {
+3        // Arrays.sort(nums);
+4        // for(int i = 0; i < nums.length; i++){
+5        //     if(nums[i] != i) return i;
+6        // }
+7        // return nums.length;
+8
+9        int n = nums.length;
+10        int sum = (n*(n+1))/2;
+11        int sumArray = 0;
+12        for(int i = 0; i < nums.length; i++){
+13            sumArray = sumArray + nums[i];
+14        }
+15        int ans = sum - sumArray;
+16        return ans;
+17    }
+18}
