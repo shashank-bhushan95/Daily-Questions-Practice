a39acb7aff0c42c9135376a14c76d0af23d27b18
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-21 13:11:16 +0530

    File updated at 6/21/2026, 1:11:14 PM

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.java
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.java
@@ -1,12 +1,13 @@
-// Last updated: 6/21/2026, 1:08:05 PM
+// Last updated: 6/21/2026, 1:11:14 PM
 1class Solution {
 2    public int[][] construct2DArray(int[] original, int m, int n) {
-3        int[][] ans = new int[m][n];
+3        
 4        if(original.length != (m*n)) return new int[0][0];
-5        for(int i = 0; i < original.length; i++){
-6            ans[i/n][i%n] = original[i];
-7        }
-8        return ans;
-9
-10    }
-11}
+5        int[][] ans = new int[m][n];
+6        for(int i = 0; i < original.length; i++){
+7            ans[i/n][i%n] = original[i];
+8        }
+9        return ans;
+10
+11    }
+12}
