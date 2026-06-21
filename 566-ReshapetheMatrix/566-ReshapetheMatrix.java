801b3ad958e92ff35a30a894186ac35e824ca2e5
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-21 15:19:47 +0530

    File updated at 6/21/2026, 3:19:45 PM

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.java
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.java
@@ -1,14 +1,11 @@
-// Last updated: 6/21/2026, 3:14:03 PM
+// Last updated: 6/21/2026, 3:19:45 PM
 1class Solution {
 2    public String reversePrefix(String s, int k) {
-3        StringBuilder sb = new StringBuilder();
-4        for(int i = k-1; i >= 0; i--){
-5            sb.append(s.charAt(i));
-6        }
-7        for(int i = k; i < s.length(); i++){
-8            sb.append(s.charAt(i));
-9        }
-10        s = sb.toString();
-11        return s;
-12    }
-13}
+3        // Step 1: Shuruat ke k characters ka substring nikal kar use reverse karo
+4        StringBuilder prefix = new StringBuilder(s.substring(0, k));
+5        prefix.reverse(); // Yeh built-in function characters ko ulta kar dega
+6        
+7        // Step 2: Bache hue part ko append karo aur string return kar do
+8        return prefix.append(s.substring(k)).toString();
+9    }
+10}
