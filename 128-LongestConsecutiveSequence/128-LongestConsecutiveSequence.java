7f024ab2273bcd8612d9569915c76beb8a50d097
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-22 10:33:07 +0530

    Create 128-LongestConsecutiveSequence

---
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -0,0 +1,20 @@
+// Last updated: 6/22/2026, 10:33:04 AM
+1class Solution {
+2    public int longestConsecutive(int[] nums) {
+3        if(nums.length == 0) return 0;
+4        int count = 1;
+5        int max = 1;
+6        Arrays.sort(nums);
+7        for(int i = 0; i < nums.length-1; i++){
+8            if(nums[i+1] - nums[i] == 1){
+9                count++;
+10                if(max < count) max = count;
+11            }
+12            else if(nums[i] == nums[i+1]) continue;
+13            if(nums[i+1] - nums[i] > 1){
+14                count = 1;
+15            }
+16        }
+17        return max;
+18    }
+19}
