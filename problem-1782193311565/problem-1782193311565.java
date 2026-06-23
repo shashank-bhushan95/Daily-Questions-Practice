98bfb334461fa58d0c22849f439937aeb2140f1d
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-23 11:11:52 +0530

    Create

---
+++ problem-1782193311565/problem-1782193311565.java
@@ -0,0 +1,11 @@
+// Last updated: 6/23/2026, 11:11:51 AM
+1class Solution {
+2    public List<Integer> targetIndices(int[] nums, int target) {
+3        Arrays.sort(nums);
+4        ArrayList<Integer> a = new ArrayList<>(); 
+5        for(int i = 0; i < nums.length; i++){
+6            if(nums[i] == target) a.add(i);
+7        }
+8        return a;
+9    }
+10}
