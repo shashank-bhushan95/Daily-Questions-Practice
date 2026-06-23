c0a246378416931ed009a80f6342f9f5d612103d
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-24 00:10:38 +0530

    File updated at 6/24/2026, 12:10:37 AM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -1,27 +1,17 @@
-// Last updated: 6/20/2026, 5:13:17 PM
+// Last updated: 6/24/2026, 12:10:37 AM
 1class Solution {
-2    public int thirdMax(int[] nums) {
-3        long m1 = Long.MIN_VALUE;
-4        long m2 = Long.MIN_VALUE;
-5        long m3 = Long.MIN_VALUE;
-6
-7        for(int i = 0; i < nums.length; i++){
-8            if(m1 == nums[i] || m2 == nums[i] || m3 == nums[i]) continue;
-9            if(nums[i] > m1){
-10                m3 = m2;
-11                m2 = m1;
-12                m1 = (long) nums[i];
-13            }
-14            else if(nums[i] > m2){
-15                m3 = m2;
-16                m2 = (long) nums[i];
-17            }
-18            else if(nums[i] > m3){
-19                m3 = (long) nums[i];
-20            }
-21        }
-22        if(m3 == Long.MIN_VALUE) return (int) m1;
-23        else return (int) m3;
-24        
-25    }
-26}
+2    public int maxConsecutive(int bottom, int top, int[] special) {
+3        Arrays.sort(special);
+4        int ans = 0;
+5        int startDiff = special[0] - bottom;
+6        ans = Math.max(ans, startDiff);
+7
+8        int lastDiff = top - special[special.length-1];
+9        ans = Math.max(ans, lastDiff);
+10        for(int i = 0; i < special.length-1; i++){
+11            int max = special[i+1] - special[i] -1;
+12            ans = Math.max(ans, max);
+13        }
+14        return ans;
+15    }
+16}
