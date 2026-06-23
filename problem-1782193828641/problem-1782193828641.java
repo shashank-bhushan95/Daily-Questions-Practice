5d38d2eec90cc80b7eec4f4614cbe729b81dcdb7
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-23 11:20:29 +0530

    Create

---
+++ problem-1782193828641/problem-1782193828641.java
@@ -0,0 +1,11 @@
+// Last updated: 6/23/2026, 11:20:28 AM
+1class Solution {
+2    public int[] searchRange(int[] nums, int target) {
+3        int[] ans = {-1, -1};
+4        for(int i = 0; i < nums.length; i++){
+5            if(nums[i] == target && ans[0] == -1) ans[0] = i;
+6            if(nums[i] == target) ans[1] = i;
+7        }
+8        return ans;
+9    }
+10}
