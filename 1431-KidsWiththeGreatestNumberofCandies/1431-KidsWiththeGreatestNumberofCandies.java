23baee183d2d2cfe0e636d85336a48b345a1acb2
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-24 00:44:04 +0530

    File updated at 6/24/2026, 12:44:03 AM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -1,17 +1,25 @@
-// Last updated: 6/24/2026, 12:10:37 AM
+// Last updated: 6/24/2026, 12:44:03 AM
 1class Solution {
-2    public int maxConsecutive(int bottom, int top, int[] special) {
-3        Arrays.sort(special);
-4        int ans = 0;
-5        int startDiff = special[0] - bottom;
-6        ans = Math.max(ans, startDiff);
-7
-8        int lastDiff = top - special[special.length-1];
-9        ans = Math.max(ans, lastDiff);
-10        for(int i = 0; i < special.length-1; i++){
-11            int max = special[i+1] - special[i] -1;
-12            ans = Math.max(ans, max);
-13        }
-14        return ans;
-15    }
-16}
+2    public int maxVowels(String s, int k) {
+3        int ans = 0;
+4        int max = 0;
+5        for(int i = 0; i < k; i++){
+6            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'){
+7                max = max + 1;
+8            }
+9        }
+10        ans = max;
+11        for(int i = k; i < s.length(); i++){
+12            if(s.charAt(i-k) == 'a' || s.charAt(i-k) == 'e' || s.charAt(i-k) == 'i' || s.charAt(i-k) == 'o' || s.charAt(i-k) == 'u') {
+13                max = max - 1;
+14            }
+15            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+16                max = max + 1;
+17            }
+18
+19            ans = Math.max(ans, max);
+20        }
+21
+22        return ans;
+23    }
+24}
