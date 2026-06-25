d6b27493889741d1097cef92ac14b24a343d09ce
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-26 00:38:53 +0530

    File updated at 6/26/2026, 12:38:52 AM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
@@ -1,22 +1,20 @@
-// Last updated: 6/26/2026, 12:22:57 AM
+// Last updated: 6/26/2026, 12:38:52 AM
 1class Solution {
 2    public List<Integer> findDisappearedNumbers(int[] nums) {
-3        ArrayList<Integer> ans = new ArrayList<>();
-4        Arrays.sort(nums);
-5        int num = 1;
-6        for(int i = 0; i < nums.length; i++){
-7            while(num < nums[i]){
-8                ans.add(num);
-9                num++;
-10            }
-11            if (i < nums.length - 1 && nums[i] == nums[i+1]) {
-12                continue; 
-13            }
-14            if(num == nums[i]) num++;
+3        Arrays.sort(nums);
+4        HashSet<Integer> hs = new HashSet<>();
+5        for(int i = 0; i < nums.length; i++){
+6            hs.add(nums[i]);
+7        }
+8
+9        ArrayList<Integer> ans = new ArrayList<>();
+10
+11        for(int i = 1; i <= nums.length; i++){
+12            if(hs.contains(i) == false){
+13                ans.add(i);
+14            }
 15        }
 16
-17        while(num <= nums.length) ans.add(num++);
-18        
-19        return ans;
-20    }
-21}
+17        return ans;
+18    }
+19}
