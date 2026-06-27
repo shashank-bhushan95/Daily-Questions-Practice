c22c0d93fcd689ba82d142542e39b03dfbe786a6
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 00:36:42 +0530

    File updated at 6/28/2026, 12:36:42 AM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,16 +1,19 @@
-// Last updated: 6/27/2026, 7:42:24 PM
+// Last updated: 6/28/2026, 12:36:42 AM
 1class Solution {
-2    public int findContentChildren(int[] g, int[] s) {
-3        Arrays.sort(g);
-4        Arrays.sort(s);
-5
-6        int gIndex = 0;
-7        // loop sirf cookies par chalega
-8        for (int sIndex = 0; gIndex < g.length && sIndex < s.length; sIndex++) {
-9            if (g[gIndex] <= s[sIndex]) {
-10                gIndex++; // agar bachha khush hua toh hi agle bachhe par jao
-11            }
-12        }
-13        return gIndex; // gIndex hi aapka final answer hoga
-14    }
-15}
+2    public int islandPerimeter(int[][] grid) {
+3        int ans = 0;
+4        int row = grid.length;
+5        int col = grid[0].length;
+6        for(int i = 0; i < row; i++){
+7            for(int j  = 0; j < col; j++){
+8                if(grid[i][j] == 1){
+9                    if(i == 0 || grid[i-1][j] == 0) ans++;
+10                    if(j == 0 || grid[i][j-1] == 0) ans++;
+11                    if(i == row-1 || grid[i+1][j] == 0) ans++;
+12                    if(j == col-1 || grid[i][j+1] == 0) ans++;
+13                }
+14            }
+15        }
+16        return ans;
+17    }
+18}
