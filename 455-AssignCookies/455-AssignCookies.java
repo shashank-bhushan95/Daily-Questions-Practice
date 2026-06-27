7f4032e35abcfcd9ad4462189ea5642280adf0a9
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 01:25:36 +0530

    File updated at 6/28/2026, 1:25:36 AM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,11 +1,17 @@
-// Last updated: 6/28/2026, 1:00:25 AM
+// Last updated: 6/28/2026, 1:25:36 AM
 1class Solution {
-2    public List<Integer> targetIndices(int[] nums, int target) {
-3        Arrays.sort(nums);
-4        ArrayList<Integer> a = new ArrayList<>(); 
-5        for(int i = 0; i < nums.length; i++){
-6            if(nums[i] == target) a.add(i);
-7        }
-8        return a;
-9    }
-10}
+2    public char repeatedCharacter(String s) {
+3        boolean[] visited = new boolean[26];
+4        for (int i = 0; i < s.length(); i++) {
+5            char ch = s.charAt(i);
+6            int index = ch - 'a';
+7            if (visited[index] == true) {
+8                return ch;
+9            }
+10            visited[index] = true;
+11        }
+12
+13        return ' ';
+14    }
+15}
+16
