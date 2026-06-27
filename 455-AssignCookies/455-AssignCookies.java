dcdd81603a0c9ea79e1c2902859c4ec703b3411e
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-27 19:42:26 +0530

    File updated at 6/27/2026, 7:42:24 PM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,25 +1,16 @@
-// Last updated: 6/27/2026, 7:35:27 PM
+// Last updated: 6/27/2026, 7:42:24 PM
 1class Solution {
 2    public int findContentChildren(int[] g, int[] s) {
 3        Arrays.sort(g);
 4        Arrays.sort(s);
 5
 6        int gIndex = 0;
-7        int sIndex = 0;
-8        int ans = 0;
-9
-10        while(gIndex < g.length && sIndex < s.length){
-11            if(g[gIndex] <= s[sIndex]){
-12                ans++;
-13                gIndex++;
-14                sIndex++;
-15            }
-16
-17            else{
-18                sIndex++;
-19            }
-20        }
-21
-22        return ans;
-23    }
-24}
+7        // loop sirf cookies par chalega
+8        for (int sIndex = 0; gIndex < g.length && sIndex < s.length; sIndex++) {
+9            if (g[gIndex] <= s[sIndex]) {
+10                gIndex++; // agar bachha khush hua toh hi agle bachhe par jao
+11            }
+12        }
+13        return gIndex; // gIndex hi aapka final answer hoga
+14    }
+15}
