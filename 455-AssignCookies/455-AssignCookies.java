e4e3089d42e5ea423efa4ecda0ab0824045e6f55
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 01:37:21 +0530

    File updated at 6/28/2026, 1:37:20 AM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,17 +1,22 @@
-// Last updated: 6/28/2026, 1:25:36 AM
-1class Solution {
-2    public char repeatedCharacter(String s) {
-3        boolean[] visited = new boolean[26];
-4        for (int i = 0; i < s.length(); i++) {
-5            char ch = s.charAt(i);
-6            int index = ch - 'a';
-7            if (visited[index] == true) {
-8                return ch;
-9            }
-10            visited[index] = true;
-11        }
-12
-13        return ' ';
-14    }
-15}
-16
+// Last updated: 6/28/2026, 1:37:20 AM
+1/* The isBadVersion API is defined in the parent class VersionControl.
+2      boolean isBadVersion(int version); */
+3
+4public class Solution extends VersionControl {
+5    public int firstBadVersion(int n) {
+6        int st = 1;
+7        int end = n;
+8        int ans = 0;
+9        while(st <= end){
+10            int mid = st + (end - st) / 2;
+11            if(isBadVersion(mid)){
+12                ans = mid;
+13                end = mid - 1;
+14            }
+15            else{
+16                st = mid + 1;
+17            }
+18        }
+19        return ans;
+20    }
+21}
