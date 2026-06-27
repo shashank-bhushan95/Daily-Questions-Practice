2d8ccd7fa853a2a23b96a3d44678713c85f65da7
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 01:00:25 +0530

    File updated at 6/28/2026, 1:00:25 AM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,19 +1,11 @@
-// Last updated: 6/28/2026, 12:36:42 AM
+// Last updated: 6/28/2026, 1:00:25 AM
 1class Solution {
-2    public int islandPerimeter(int[][] grid) {
-3        int ans = 0;
-4        int row = grid.length;
-5        int col = grid[0].length;
-6        for(int i = 0; i < row; i++){
-7            for(int j  = 0; j < col; j++){
-8                if(grid[i][j] == 1){
-9                    if(i == 0 || grid[i-1][j] == 0) ans++;
-10                    if(j == 0 || grid[i][j-1] == 0) ans++;
-11                    if(i == row-1 || grid[i+1][j] == 0) ans++;
-12                    if(j == col-1 || grid[i][j+1] == 0) ans++;
-13                }
-14            }
-15        }
-16        return ans;
-17    }
-18}
+2    public List<Integer> targetIndices(int[] nums, int target) {
+3        Arrays.sort(nums);
+4        ArrayList<Integer> a = new ArrayList<>(); 
+5        for(int i = 0; i < nums.length; i++){
+6            if(nums[i] == target) a.add(i);
+7        }
+8        return a;
+9    }
+10}
