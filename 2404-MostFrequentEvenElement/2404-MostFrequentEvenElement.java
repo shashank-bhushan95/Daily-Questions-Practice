adf07947b8ed44f6febe08e2d8d72ff7e8b62dd4
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 23:26:09 +0530

    File updated at 6/28/2026, 11:26:09 PM

--- 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
+++ 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
@@ -1,24 +1,26 @@
-// Last updated: 6/28/2026, 11:24:28 PM
+// Last updated: 6/28/2026, 11:26:09 PM
 1class Solution {
 2    public int mostFrequentEven(int[] nums) {
-3        if(nums.length == 1 && nums[0]%2==0) return nums[0];
-4        Arrays.sort(nums);
-5        int ans = -1;
-6        int countMax = 0;
-7        for(int i = 0; i < nums.length; i++){
+3        Arrays.sort(nums);
+4        int ans = -1;
+5        int maxFreq = 0;
+6        int i = 0;
+7        while (i < nums.length) {
 8            int count = 1;
-9            if(nums[i] % 2 != 0) continue;
-10            for(int j = i+1; j < nums.length; j++){
-11                if(nums[i] == nums[j]){
-12                    count++;
-13                }
-14                else break;
-15            }
-16            if (count > countMax) {
-17                ans = nums[i];
-18                countMax = count;
-19            }
-20        }
-21        return ans;
-22    }
-23}
+9            int num = nums[i];
+10
+11            while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+12                count++;
+13                i++;
+14            }
+15            if (num % 2 == 0) {
+16                if (count > maxFreq) {
+17                    maxFreq = count;
+18                    ans = num;
+19                }
+20            }
+21            i++;
+22        }
+23        return ans;
+24    }
+25}   
