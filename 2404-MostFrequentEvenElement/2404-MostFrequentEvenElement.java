e93a79650b7b8285cf6d5759859063605eab527c
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 23:24:28 +0530

    File updated at 6/28/2026, 11:24:28 PM

--- 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
+++ 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
@@ -1,22 +1,22 @@
-// Last updated: 6/28/2026, 11:06:40 PM
+// Last updated: 6/28/2026, 11:24:28 PM
 1class Solution {
 2    public int mostFrequentEven(int[] nums) {
-3        int ans = -1;
-4        int maxFreq = 0;
-5        HashMap<Integer, Integer> hm = new HashMap<>();
-6        for(int i = 0; i < nums.length; i++){
-7            if(nums[i] % 2 == 0){
-8                hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
-9            }
-10        }
-11
-12        for (Map.Entry<Integer, Integer> e : hm.entrySet()) {
-13            int currentNum = e.getKey();
-14            int currentFreq = e.getValue();
-15
-16            if (currentFreq > maxFreq || (currentFreq == maxFreq && currentNum < ans)) {
-17                maxFreq = currentFreq;
-18                ans = currentNum;
+3        if(nums.length == 1 && nums[0]%2==0) return nums[0];
+4        Arrays.sort(nums);
+5        int ans = -1;
+6        int countMax = 0;
+7        for(int i = 0; i < nums.length; i++){
+8            int count = 1;
+9            if(nums[i] % 2 != 0) continue;
+10            for(int j = i+1; j < nums.length; j++){
+11                if(nums[i] == nums[j]){
+12                    count++;
+13                }
+14                else break;
+15            }
+16            if (count > countMax) {
+17                ans = nums[i];
+18                countMax = count;
 19            }
 20        }
 21        return ans;
