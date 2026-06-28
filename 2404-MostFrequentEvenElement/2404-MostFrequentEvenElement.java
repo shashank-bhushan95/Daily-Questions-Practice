437d6ff9226ceaad90f90cb69509dbe6d5b5642c
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-28 23:58:13 +0530

    File updated at 6/28/2026, 11:58:13 PM

--- 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
+++ 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
@@ -1,26 +1,14 @@
-// Last updated: 6/28/2026, 11:26:09 PM
+// Last updated: 6/28/2026, 11:58:13 PM
 1class Solution {
-2    public int mostFrequentEven(int[] nums) {
-3        Arrays.sort(nums);
-4        int ans = -1;
-5        int maxFreq = 0;
-6        int i = 0;
-7        while (i < nums.length) {
-8            int count = 1;
-9            int num = nums[i];
-10
-11            while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-12                count++;
-13                i++;
-14            }
-15            if (num % 2 == 0) {
-16                if (count > maxFreq) {
-17                    maxFreq = count;
-18                    ans = num;
-19                }
-20            }
-21            i++;
-22        }
-23        return ans;
-24    }
-25}   
+2    public boolean containsNearbyDuplicate(int[] nums, int k) {
+3        HashSet<Integer> h = new HashSet<>();
+4        for(int i = 0; i < nums.length; i++){
+5            if(h.contains(nums[i])) return true;
+6            h.add(nums[i]);
+7            if(h.size() > k){
+8                h.remove(nums[i-k]);
+9            }
+10        }
+11        return false;
+12    }
+13}
