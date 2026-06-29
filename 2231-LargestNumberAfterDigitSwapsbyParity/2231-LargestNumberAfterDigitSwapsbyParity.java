92acc153d619c7a920b758642248b22fddda0b78
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-06-30 00:27:23 +0530

    File updated at 6/30/2026, 12:27:22 AM

--- 2231-LargestNumberAfterDigitSwapsbyParity/2231-LargestNumberAfterDigitSwapsbyParity.java
+++ 2231-LargestNumberAfterDigitSwapsbyParity/2231-LargestNumberAfterDigitSwapsbyParity.java
@@ -1,28 +1,24 @@
-// Last updated: 6/30/2026, 12:13:49 AM
+// Last updated: 6/30/2026, 12:27:22 AM
 1class Solution {
-2    public int flipgame(int[] fronts, int[] backs) {
-3        // Since max possible card value is 2000, we use a fixed boolean array
-4        boolean[] isBanned = new boolean[2001];
-5        int n = fronts.length;
-6        for (int i = 0; i < n; i++) {
-7            if (fronts[i] == backs[i]) {
-8                isBanned[fronts[i]] = true;
-9            }
-10        }
-11
-12        int ans = Integer.MAX_VALUE;
-13        for (int i = 0; i < n; i++) {
-14            if (fronts[i] < ans && !isBanned[fronts[i]]) {
-15                ans = fronts[i];
-16            }
-17            if (backs[i] < ans && !isBanned[backs[i]]) {
-18                ans = backs[i];
-19            }
-20        }
-21
-22        if(ans == Integer.MAX_VALUE) return 0;
-23        return ans;
-24        
-25    }
-26}
-27
+2    public int[][] flipAndInvertImage(int[][] image) {
+3        for(int i = 0; i < image.length; i++){
+4            int start = 0;
+5            int end = image[0].length -1;
+6            while(start < end){
+7                int temp = image[i][start];
+8                image[i][start] = image[i][end];
+9                image[i][end] = temp;
+10                start++;
+11                end--;
+12            }
+13        }
+14        for(int i = 0; i < image.length; i++){
+15            for(int j = 0; j < image[0].length; j++){
+16                if(image[i][j] == 1) image[i][j] = 0;
+17                else image[i][j] = 1;
+18            }
+19        }
+20
+21        return image;
+22    }
+23}
