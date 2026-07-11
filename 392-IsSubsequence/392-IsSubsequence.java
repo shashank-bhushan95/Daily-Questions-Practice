c3fc47683c427b0071729fd6f6f90660f1c52e36
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-11 14:19:02 +0530

    File updated at 7/11/2026, 2:19:00 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,19 +1,26 @@
-// Last updated: 7/11/2026, 1:55:59 PM
+// Last updated: 7/11/2026, 2:19:00 PM
 1class Solution {
-2    public int[] sortArrayByParity(int[] nums) {
-3        int i = 0;
-4        int j = nums.length-1;
-5        while(i < j){
-6            if(nums[i] % 2 != 0 && nums[j] % 2 == 0){
-7                int temp = nums[i];
-8                nums[i] = nums[j];
-9                nums[j] = temp;
-10                i++;
-11                j--;
-12            }
-13            else if(nums[i] % 2 == 0) i++;
-14            else if(nums[j] % 2 == 1) j--;
-15        }
-16        return nums;
-17    }
-18}
+2    public String firstPalindrome(String[] words) {
+3        String ans = "";
+4        for(int i = 0; i < words.length; i++){
+5            int start = 0;
+6            int end = words[i].length()-1;
+7            boolean flag = true;
+8            while(start < end){
+9                if(words[i].charAt(start) != words[i].charAt(end)){
+10                    flag = false;
+11                    break;
+12                }
+13                else{
+14                    start++;
+15                    end--;
+16                }
+17            }
+18            if(flag){
+19                ans = words[i];
+20                return ans;
+21            }
+22        }
+23        return ans;
+24    }
+25}
