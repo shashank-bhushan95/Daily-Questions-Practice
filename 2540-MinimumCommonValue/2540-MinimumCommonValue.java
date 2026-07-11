5025b4c2d09c30a70e19cb6bfecd0e482cc64e85
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-12 01:16:10 +0530

    File updated at 7/12/2026, 1:16:10 AM

--- 2540-MinimumCommonValue/2540-MinimumCommonValue.java
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.java
@@ -1,14 +1,17 @@
-// Last updated: 7/11/2026, 3:05:23 PM
+// Last updated: 7/12/2026, 1:16:10 AM
 1class Solution {
-2    public int getCommon(int[] nums1, int[] nums2) {
-3        int indx1 = 0;
-4        int indx2 = 0;
-5        while(indx1 < nums1.length && indx2 < nums2.length){
-6            if(nums1[indx1] == nums2[indx2]) return nums1[indx1];
-7            else if(nums1[indx1] < nums2[indx2]) indx1++;
-8            else if(nums1[indx1] > nums2[indx2]) indx2++;
-9        }
-10        //while()
-11        return -1;
-12    }
-13}
+2    public String longestCommonPrefix(String[] strs) {
+3        if (strs == null || strs.length == 0) return "";
+4        for (int i = 0; i < strs[0].length(); i++) {
+5            char c = strs[0].charAt(i);
+6            for (int j = 1; j < strs.length; j++) {
+7                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+8                    return strs[0].substring(0, i);
+9                }
+10            }
+11        }
+12        
+13        return strs[0];
+14    }
+15}
+16
