859ccf892226d1002afa75e309de1052d02bd64b
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-11 14:24:18 +0530

    File updated at 7/11/2026, 2:24:16 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,26 +1,22 @@
-// Last updated: 7/11/2026, 2:19:00 PM
+// Last updated: 7/11/2026, 2:24:16 PM
 1class Solution {
-2    public String firstPalindrome(String[] words) {
-3        String ans = "";
-4        for(int i = 0; i < words.length; i++){
-5            int start = 0;
-6            int end = words[i].length()-1;
-7            boolean flag = true;
-8            while(start < end){
-9                if(words[i].charAt(start) != words[i].charAt(end)){
-10                    flag = false;
-11                    break;
-12                }
-13                else{
-14                    start++;
-15                    end--;
-16                }
+2    public int[] applyOperations(int[] nums) {
+3        for(int i = 0; i < nums.length-1; i++){
+4            if(nums[i] == nums[i+1]){
+5                nums[i] = nums[i] * 2;
+6                nums[i+1] = 0;
+7            }
+8        }
+9
+10        int k = 0;
+11        for(int i = 0; i < nums.length; i++){
+12            if(nums[i] != 0){
+13                int temp = nums[i];
+14                nums[i] = nums[k];
+15                nums[k] = temp;
+16                k++;
 17            }
-18            if(flag){
-19                ans = words[i];
-20                return ans;
-21            }
-22        }
-23        return ans;
-24    }
-25}
+18        }
+19        return nums;
+20    }
+21}
