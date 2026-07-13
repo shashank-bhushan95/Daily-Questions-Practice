966c9a3cdfbe1a773d11d6e487f3a5242a5a4ea2
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-13 12:25:07 +0530

    File updated at 7/13/2026, 12:25:06 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,28 +1,23 @@
-// Last updated: 7/13/2026, 11:25:28 AM
+// Last updated: 7/13/2026, 12:25:06 PM
 1class Solution {
 2    public int strStr(String haystack, String needle) {
-3        if (needle.length() == 0) return 0;
-4        int i = 0;
+3        if(needle.length() == 0) return -1;
+4        int count  = 0;
 5        int j = 0;
-6        int indx = 0;
-7        while(i < haystack.length()){
-8            if(haystack.charAt(i) != needle.charAt(j)) i++;
-9            else{
-10                indx = i;
-11                while(j < needle.length() && i < haystack.length()){
-12                    if(needle.charAt(j) != haystack.charAt(i)) {
-13                        j = 0;
-14                        i = indx+1;
-15                        break;
-16                    }
-17                    else{
-18                        i++;
-19                        j++;
-20                    }
-21                }
-22                if(j == needle.length()) return i-needle.length();
-23            }
-24        }
-25        return -1;
-26    }
-27}
+6        for(int i = 0; i < haystack.length(); i++){
+7            if(haystack.charAt(i) != needle.charAt(j)){
+8                if (count > 0) {
+9                    i = i - count;
+10                }
+11                j = 0;
+12                count = 0;
+13            }
+14            else{
+15                j++;
+16                count++;
+17                if(count == needle.length()) return i-needle.length()+1;
+18            }
+19        }
+20        return -1;
+21    }
+22}
