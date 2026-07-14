cff0d651683ba363221c464de2863a09115e8d64
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-14 22:41:15 +0530

    File updated at 7/14/2026, 10:41:14 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,21 +1,15 @@
-// Last updated: 11/28/2025, 10:44:54 AM
+// Last updated: 7/14/2026, 10:41:14 PM
 1class Solution {
-2    public int maxProfit(int[] prices) {
-3        int[] left = new int[prices.length];
-4        int right[] = new int[prices.length];
-5        left[0] = prices[0];
-6        right[prices.length-1] = prices[prices.length-1];
-7        for(int i = 1; i < prices.length; i++){
-8            left[i] = Math.min(left[i-1], prices[i]);
-9        }
-10        for(int i = prices.length-2; i >= 0; i--){
-11            right[i] = Math.max(right[i+1], prices[i]);
-12        }
-13        int max = 0;
-14        for(int i = 0; i < prices.length; i++){
-15            max = Math.max(max, right[i]-left[i]);
-16        }
-17        return max;
-18
-19    }
-20}
+2    public int singleNumber(int[] nums) {
+3        HashMap<Integer, Integer> hm = new HashMap<>();
+4        for(int i = 0; i < nums.length; i++){
+5            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+6        }
+7
+8        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+9            if(entry.getValue() == 1) return entry.getKey();
+10            //System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+11        }
+12        return 0;
+13    }
+14}
