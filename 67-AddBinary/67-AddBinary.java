46662861c0ed8da2eb43f10df6266264c24ba013
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-14 11:09:16 +0530

    File updated at 7/14/2026, 11:09:15 AM

--- 67-AddBinary/67-AddBinary.java
+++ 67-AddBinary/67-AddBinary.java
@@ -1,24 +1,19 @@
-// Last updated: 7/14/2026, 11:07:23 AM
+// Last updated: 7/14/2026, 11:09:15 AM
 1class Solution {
 2    public int maxProfit(int[] prices) {
-3        int length = prices.length;
-4        int[] min = new int[length];
-5        int[] max = new int[length];
-6        min[0] = prices[0];
-7        max[length-1] = prices[length-1];
-8        for(int i = 1; i < length; i++){
-9            min[i] = Math.min(min[i-1], prices[i]);
-10        }
-11
-12        for(int i = length - 2; i >= 0; i--){
-13            max[i] = Math.max(max[i+1], prices[i]);
+3        if (prices == null || prices.length == 0) {
+4            return 0;
+5        }
+6
+7        int minPrice = prices[0];
+8        int maxProfit = 0;
+9
+10        for (int i = 1; i < prices.length; i++) {
+11            int currentProfit = prices[i] - minPrice;
+12            maxProfit = Math.max(maxProfit, currentProfit);
+13            minPrice = Math.min(minPrice, prices[i]);
 14        }
 15
-16        int maxV = 0;
-17        for(int i = 0; i < prices.length; i++){
-18            maxV = Math.max(maxV, max[i]-min[i]);
-19        }
-20
-21        return maxV;
-22    }
-23}
+16        return maxProfit;
+17    }
+18}
