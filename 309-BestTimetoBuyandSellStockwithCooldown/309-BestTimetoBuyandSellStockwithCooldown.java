95b3a55da57a411070294ae2fcac0de842847a43
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-15 12:33:23 +0530

    File updated at 7/15/2026, 12:33:22 PM

--- 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.java
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.java
@@ -1,33 +1,24 @@
-// Last updated: 7/15/2026, 12:27:57 PM
+// Last updated: 7/15/2026, 12:33:22 PM
 1class Solution {
 2    public int[] intersect(int[] nums1, int[] nums2) {
-3        Arrays.sort(nums1);
-4        Arrays.sort(nums2);
-5        ArrayList<Integer> arr = new ArrayList<>();
-6
-7        int i = 0;
-8        int j = 0;
-9        while(i < nums1.length && j < nums2.length){
-10            if(nums1[i] == nums2[j]){ 
-11                arr.add(nums1[i]);
-12                i++;
-13                j++;
-14            }
-15            
-16            else if(nums1[i] > nums2[j]){
-17                j++;
-18            }
-19            else if(nums1[i] < nums2[j]){
-20                i++;
-21            }
-22        }
-23
-24        int[] ans = new int[arr.size()];
-25        int index = 0;
-26        for(int num : arr){
-27            ans[index++] = num;
-28        }
-29
-30        return ans;
-31    }
-32}
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        ArrayList<Integer> list = new ArrayList<>();
+5
+6        for (int num : nums1) {
+7            map.put(num, map.getOrDefault(num, 0) + 1);
+8        }
+9        for (int num : nums2) {
+10            if (map.containsKey(num) && map.get(num) > 0) {
+11                list.add(num);
+12                map.put(num, map.get(num) - 1);
+13            }
+14        }
+15        int[] ans = new int[list.size()];
+16        int index = 0;
+17        for (int num : list) {
+18            ans[index++] = num;
+19        }
+20
+21        return ans;
+22    }
+23}
