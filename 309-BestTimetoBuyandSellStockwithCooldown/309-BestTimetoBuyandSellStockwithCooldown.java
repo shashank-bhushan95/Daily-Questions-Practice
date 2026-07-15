aedd91ec5d846816bc680c82296f1913a173153f
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-15 12:49:53 +0530

    File updated at 7/15/2026, 12:49:53 PM

--- 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.java
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.java
@@ -1,24 +1,20 @@
-// Last updated: 7/15/2026, 12:33:22 PM
+// Last updated: 7/15/2026, 12:49:53 PM
 1class Solution {
-2    public int[] intersect(int[] nums1, int[] nums2) {
-3        HashMap<Integer, Integer> map = new HashMap<>();
-4        ArrayList<Integer> list = new ArrayList<>();
-5
-6        for (int num : nums1) {
-7            map.put(num, map.getOrDefault(num, 0) + 1);
-8        }
-9        for (int num : nums2) {
-10            if (map.containsKey(num) && map.get(num) > 0) {
-11                list.add(num);
-12                map.put(num, map.get(num) - 1);
-13            }
-14        }
-15        int[] ans = new int[list.size()];
-16        int index = 0;
-17        for (int num : list) {
-18            ans[index++] = num;
-19        }
-20
-21        return ans;
-22    }
-23}
+2    public boolean isPerfectSquare(int num) {
+3        long start = 0;
+4        long end = num;
+5        
+6        while(start <= end){
+7            long mid = (start + end) / 2;
+8            long pro = mid * mid;
+9            if(pro == num) return true;
+10            else if(pro > num){
+11                end = mid-1;
+12            }
+13            else{
+14                start = mid+1;
+15            }
+16        }
+17        return false;
+18    }
+19}
