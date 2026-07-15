252e18bf6c59f119a1e864f04b39354903adbcde
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-15 10:22:59 +0530

    File updated at 7/15/2026, 10:22:59 AM

--- 202-HappyNumber/202-HappyNumber.java
+++ 202-HappyNumber/202-HappyNumber.java
@@ -1,18 +1,19 @@
-// Last updated: 7/15/2026, 9:35:41 AM
+// Last updated: 7/15/2026, 10:22:59 AM
 1class Solution {
-2    public boolean isHappy(int n) {
-3        ArrayList<Integer> arr = new ArrayList<>();
-4        while(arr.contains(n) == false && n != 1){
-5            int squareSum = 0;
-6            arr.add(n);
-7            while(n > 0){
-8                int digit = n % 10;
-9                squareSum = squareSum + (digit * digit);
-10                n = n / 10;
+2    public boolean isIsomorphic(String s, String t) {
+3        
+4        HashMap<Character, Integer> mapS = new HashMap<>();
+5        HashMap<Character, Integer> mapT = new HashMap<>();
+6        for(int i = 0; i < s.length(); i++){
+7            char charS = s.charAt(i);
+8            char charT = t.charAt(i);
+9            if (!mapS.getOrDefault(charS, -1).equals(mapT.getOrDefault(charT, -1))) {
+10                return false;
 11            }
-12            n = squareSum;
-13        }
-14        if(n == 1) return true;
-15        return false;
-16    }
-17}
+12            mapS.put(charS, i);
+13            mapT.put(charT, i);
+14        }
+15
+16        return true;
+17    }
+18}
