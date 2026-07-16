7d88d26d946e54e28616380b2fe0692909bdd66b
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-16 10:22:59 +0530

    File updated at 7/16/2026, 10:22:58 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,21 +1,29 @@
-// Last updated: 7/16/2026, 9:49:43 AM
+// Last updated: 7/16/2026, 10:22:58 AM
 1class Solution {
-2    public boolean canConstruct(String ransomNote, String magazine) {
-3
-4        HashMap<Character, Integer> map = new HashMap<>();
-5        for (int i = 0; i < magazine.length(); i++) {
-6            char ch = magazine.charAt(i);
-7            map.put(ch, map.getOrDefault(ch, 0) + 1);
-8        }
-9
-10        for (int i = 0; i < ransomNote.length(); i++) {
-11            char ch = ransomNote.charAt(i);
-12            if (!map.containsKey(ch) || map.get(ch) == 0) {
-13                return false;
-14            }
-15            map.put(ch, map.get(ch) - 1);
-16        }
-17
-18        return true;
-19    }
-20}
+2    public int firstUniqChar(String s) {
+3        Queue<Character> q = new ArrayDeque<>();
+4        HashSet<Character> duplicates = new HashSet<>();
+5        for(int i = 0; i < s.length(); i++){
+6            char ch = s.charAt(i);
+7            if (!q.contains(ch) && !duplicates.contains(ch)) {
+8                q.add(ch);
+9            } 
+10            else {
+11                q.remove(ch);
+12                duplicates.add(ch);
+13            }
+14        }
+15        if(q.size() > 0){
+16            int ans = 0;
+17            char ansch = q.poll();
+18            for(int i = 0; i < s.length(); i++){
+19                if(ansch == s.charAt(i)){
+20                    ans = i;
+21                    break;
+22                }
+23            }
+24            return ans;
+25        }
+26        return -1;
+27    }
+28}
