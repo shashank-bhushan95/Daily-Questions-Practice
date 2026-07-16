c4ea8f7be65ac7ed4bde9da5c0a866f5632a9123
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-16 09:49:43 +0530

    Create 219-ContainsDuplicateII

---
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -0,0 +1,21 @@
+// Last updated: 7/16/2026, 9:49:43 AM
+1class Solution {
+2    public boolean canConstruct(String ransomNote, String magazine) {
+3
+4        HashMap<Character, Integer> map = new HashMap<>();
+5        for (int i = 0; i < magazine.length(); i++) {
+6            char ch = magazine.charAt(i);
+7            map.put(ch, map.getOrDefault(ch, 0) + 1);
+8        }
+9
+10        for (int i = 0; i < ransomNote.length(); i++) {
+11            char ch = ransomNote.charAt(i);
+12            if (!map.containsKey(ch) || map.get(ch) == 0) {
+13                return false;
+14            }
+15            map.put(ch, map.get(ch) - 1);
+16        }
+17
+18        return true;
+19    }
+20}
