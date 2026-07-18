6ff30016e227a9956466fd33ed4bc6555d950c78
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-19 00:35:20 +0530

    File updated at 7/19/2026, 12:35:20 AM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.java
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -1,28 +1,14 @@
-// Last updated: 7/19/2026, 12:35:02 AM
+// Last updated: 7/19/2026, 12:35:20 AM
 1class Solution {
-2    public boolean isPalindrome(int x) {
-3
-4        // String s = x + "";
-5        // int i = 0;
-6        // int j = s.length()-1;
-7        // while(i < j){
-8        //     if(s.charAt(i) != s.charAt(j)){
-9        //         return false;
-10        //     }
-11        //     i++;
-12        //     j--;
-13        // }
-14        // return true;
-15
-16        int rev = 0;
-17        int copy = x;
-18        int digit = 0;
-19        while(copy > 0){
-20            digit = copy % 10;
-21            rev = rev * 10 + digit;
-22            copy = copy / 10;
-23        }
-24        if(rev == x) return true;
-25        return false;
-26    }
-27}
+2    public int removeElement(int[] nums, int val) {
+3        int index = 0;
+4
+5        for(int i = 0; i < nums.length; i++){
+6            if(nums[i] == val) continue;
+7            else{
+8                nums[index++] = nums[i];
+9            }
+10        }
+11        return index;
+12    }
+13}
