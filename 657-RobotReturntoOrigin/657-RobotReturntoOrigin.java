a33c1ee50f83baeb457052be5fbb4d6876513b8f
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-19 15:40:15 +0530

    File updated at 7/19/2026, 3:40:13 PM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
@@ -1,20 +1,25 @@
-// Last updated: 7/19/2026, 3:19:13 PM
+// Last updated: 7/19/2026, 3:40:13 PM
 1class Solution {
-2    public int maximumWealth(int[][] accounts) {
-3        int maxWealth = 0;
-4        
-5        for (int[] customer : accounts) {
-6            int currentWealth = 0;
-7            for (int bankBalance : customer) {
-8                currentWealth += bankBalance;
-9            }
-10            // Update maxWealth if the current customer is richer
-11            if (currentWealth > maxWealth) {
-12                maxWealth = currentWealth;
-13            }
-14        }
-15        
-16        return maxWealth;
-17    }
-18}
-19
+2    public int value(char ch){
+3        if(ch == 'I') return 1;
+4        if(ch == 'V') return 5;
+5        if(ch == 'X') return 10;
+6        if(ch == 'L') return 50;
+7        if(ch == 'C') return 100;
+8        if(ch == 'D') return 500;
+9        if(ch == 'M') return 1000;
+10        return 0;
+11    }
+12    public int romanToInt(String s) {
+13        int total = 0;
+14        for(int i = 0; i < s.length(); i++){
+15            char ch = s.charAt(i);
+16            int val = value(ch);
+17            if(i+1 < s.length() && val < value(s.charAt(i+1))){
+18                total -= val;
+19            }
+20            else total += val;
+21        }
+22        return total;
+23    }
+24}
