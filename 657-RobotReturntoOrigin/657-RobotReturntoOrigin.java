d143483ba2df18e591d7af2647fba1490226cefc
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-19 14:55:29 +0530

    File updated at 7/19/2026, 2:55:28 PM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
@@ -1,17 +1,15 @@
-// Last updated: 7/19/2026, 2:53:16 PM
+// Last updated: 7/19/2026, 2:55:28 PM
 1class Solution {
 2    public boolean judgeCircle(String moves) {
 3        int l = 0;
-4        int r = 0;
-5        int d = 0;
-6        int u = 0;
-7        for(int i = 0; i < moves.length(); i++){
-8            if(moves.charAt(i) == 'U') u++;
-9            else if(moves.charAt(i) == 'L') l++;
-10            else if(moves.charAt(i) == 'R') r++;
-11            else d++;
-12        }
-13        if((l == r) && d == u) return true;
-14        return false; 
-15    }
-16}
+4        int u = 0;
+5        for(int i = 0; i < moves.length(); i++){
+6            if(moves.charAt(i) == 'U') u++;
+7            else if(moves.charAt(i) == 'D') u--;
+8            else if(moves.charAt(i) == 'L') l++;
+9            else l--;
+10        }
+11        if((l == 0) && u == 0) return true;
+12        return false; 
+13    }
+14}
