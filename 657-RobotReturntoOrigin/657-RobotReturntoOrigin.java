383612cdff66cc33ddd0c38d17cd9b861dcedf84
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-19 15:19:14 +0530

    File updated at 7/19/2026, 3:19:13 PM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
@@ -1,14 +1,20 @@
-// Last updated: 7/19/2026, 3:17:19 PM
+// Last updated: 7/19/2026, 3:19:13 PM
 1class Solution {
 2    public int maximumWealth(int[][] accounts) {
-3        int max = 0;
-4        for(int i = 0; i < accounts.length; i++){
-5            int sum = 0;
-6            for(int j = 0; j < accounts[0].length; j++){
-7                sum += accounts[i][j];
-8            }
-9            max = Math.max(max, sum);
-10        }
-11        return max;
-12    }
-13}
+3        int maxWealth = 0;
+4        
+5        for (int[] customer : accounts) {
+6            int currentWealth = 0;
+7            for (int bankBalance : customer) {
+8                currentWealth += bankBalance;
+9            }
+10            // Update maxWealth if the current customer is richer
+11            if (currentWealth > maxWealth) {
+12                maxWealth = currentWealth;
+13            }
+14        }
+15        
+16        return maxWealth;
+17    }
+18}
+19
