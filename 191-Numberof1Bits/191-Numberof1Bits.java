69ef90303834f3015b2a4f8dba1aa3b001f8e2e1
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-20 23:12:30 +0530

    File updated at 7/20/2026, 11:12:30 PM

--- 191-Numberof1Bits/191-Numberof1Bits.java
+++ 191-Numberof1Bits/191-Numberof1Bits.java
@@ -1,7 +1,13 @@
-// Last updated: 7/20/2026, 1:39:02 AM
+// Last updated: 7/20/2026, 11:12:30 PM
 1class Solution {
 2    public int hammingWeight(int n) {
-3        //int decimal = Integer.parseInt(n, 2);
-4        return Integer.bitCount(n);
-5    }
-6}
+3        //return Integer.bitCount(n);
+4        int ans = 0;
+5        while(n > 0){
+6            int digit = n % 2;
+7            if(digit == 1) ans++;
+8            n = n / 2;
+9        }
+10        return ans;
+11    }
+12}
