af4730b68b10863894fb1530a0131aa209a89d1a
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-21 00:34:41 +0530

    File updated at 7/21/2026, 12:34:40 AM

--- 191-Numberof1Bits/191-Numberof1Bits.java
+++ 191-Numberof1Bits/191-Numberof1Bits.java
@@ -1,13 +1,30 @@
-// Last updated: 7/20/2026, 11:12:30 PM
-1class Solution {
-2    public int hammingWeight(int n) {
-3        //return Integer.bitCount(n);
-4        int ans = 0;
-5        while(n > 0){
-6            int digit = n % 2;
-7            if(digit == 1) ans++;
-8            n = n / 2;
-9        }
-10        return ans;
-11    }
-12}
+// Last updated: 7/21/2026, 12:34:40 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeElements(ListNode head, int val) {
+13        if(head == null) return head;
+14
+15        ListNode dummy = new ListNode(0);
+16        dummy.next = head;
+17        ListNode curr = dummy;
+18        
+19        while(curr.next != null){
+20            if(curr.next.val == val){
+21                curr.next = curr.next.next;
+22            }
+23            else{
+24                curr = curr.next;
+25            }
+26        }  
+27        return dummy.next; 
+28    }
+29}
