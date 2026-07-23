37c449f57e91776066f13e917cc5aa2d61bde3f2
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-23 13:52:22 +0530

    File updated at 7/23/2026, 1:52:21 PM

--- 409-LongestPalindrome/409-LongestPalindrome.java
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -1,28 +1,40 @@
-// Last updated: 7/23/2026, 2:07:47 AM
+// Last updated: 7/23/2026, 1:52:21 PM
 1class Solution {
-2    public int longestPalindrome(String s) {
-3    
-4        int[] arr = new int[128];
-5        for(int i = 0; i < s.length(); i++){
-6            char ch = s.charAt(i);
-7            int index = (int)(ch);
-8            arr[index]++;
-9        }
-10
-11        boolean odd = false;
-12        int ans = 0;
-13        for(int num : arr){
-14            if(num % 2 == 0) ans += num;
-15            else if(num % 2 != 0){
-16                ans += num-1;
-17                odd = true;
-18            }
-19        }
-20
-21        if(odd){
-22            ans++;
-23        }
-24
-25        return ans;
-26    }
-27}
+2    public int hammingDistance(int x, int y) {
+3        ArrayList<Integer> arrX = new ArrayList<>();
+4        ArrayList<Integer> arrY = new ArrayList<>();
+5        if(x == 0) arrX.add(0);
+6        if(y == 0) arrY.add(0);
+7
+8        while(x > 0){
+9            int digit = x % 2;
+10            arrX.add(digit);
+11            x = x / 2;
+12        }
+13
+14        while(y > 0){
+15            int digit = y % 2;
+16            arrY.add(digit);
+17            y = y / 2;
+18        }
+19
+20        if(arrX.size() > arrY.size()){
+21            while(arrX.size() != arrY.size()){
+22                arrY.add(0);
+23            }
+24        }
+25        else{
+26            while(arrX.size() != arrY.size()){
+27                arrX.add(0);
+28            }
+29        }
+30
+31        int count = 0;
+32        for (int i = 0; i < arrX.size(); i++) {
+33            if (!arrX.get(i).equals(arrY.get(i))) {
+34                count++;
+35            }
+36        }
+37        return count;
+38    }
+39}
