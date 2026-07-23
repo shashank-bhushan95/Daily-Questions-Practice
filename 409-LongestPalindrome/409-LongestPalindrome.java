dbf1ef63143ec578e9becffbc917171c2e87942d
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-23 14:12:54 +0530

    File updated at 7/23/2026, 2:12:53 PM

--- 409-LongestPalindrome/409-LongestPalindrome.java
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -1,40 +1,25 @@
-// Last updated: 7/23/2026, 1:52:21 PM
+// Last updated: 7/23/2026, 2:12:53 PM
 1class Solution {
-2    public int hammingDistance(int x, int y) {
-3        ArrayList<Integer> arrX = new ArrayList<>();
-4        ArrayList<Integer> arrY = new ArrayList<>();
-5        if(x == 0) arrX.add(0);
-6        if(y == 0) arrY.add(0);
-7
-8        while(x > 0){
-9            int digit = x % 2;
-10            arrX.add(digit);
-11            x = x / 2;
-12        }
-13
-14        while(y > 0){
-15            int digit = y % 2;
-16            arrY.add(digit);
-17            y = y / 2;
-18        }
-19
-20        if(arrX.size() > arrY.size()){
-21            while(arrX.size() != arrY.size()){
-22                arrY.add(0);
-23            }
-24        }
-25        else{
-26            while(arrX.size() != arrY.size()){
-27                arrX.add(0);
-28            }
-29        }
-30
-31        int count = 0;
-32        for (int i = 0; i < arrX.size(); i++) {
-33            if (!arrX.get(i).equals(arrY.get(i))) {
-34                count++;
-35            }
-36        }
-37        return count;
-38    }
-39}
+2    public int findComplement(int num) {
+3        if(num == 0) return 1;
+4        boolean even = false;
+5        if(num % 2 == 0) even = true;
+6        ArrayList<Integer> arr = new ArrayList<>();
+7        while(num > 0){
+8            int digit = num % 2;
+9            arr.add(digit);
+10            num = num / 2;
+11        }
+12        //
+13        //Collections.reverse(arr);
+14        int ans = 0;
+15        int exp = 0;    // Use for power
+16        for(int n : arr){
+17            if(n == 0){
+18                ans = ans + (int) Math.pow(2, exp);
+19            }
+20            exp++;
+21        }
+22        return ans;
+23    }
+24}
