5eebf065d8adbf3428f792f5a1b0e62c9691faf9
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-25 23:40:26 +0530

    File updated at 7/25/2026, 11:40:25 PM

--- 500-KeyboardRow/500-KeyboardRow.java
+++ 500-KeyboardRow/500-KeyboardRow.java
@@ -1,16 +1,23 @@
-// Last updated: 7/25/2026, 11:04:20 PM
+// Last updated: 7/25/2026, 11:40:25 PM
 1class Solution {
-2    public boolean detectCapitalUse(String word) {
-3        int low = 0;
-4        int upp = 0;
-5        for(int i = 0; i < word.length(); i++){
-6            char ch = word.charAt(i);
-7            if(ch >= 'a' && ch <= 'z') low++;
-8            else upp++;
-9        }
-10        if (upp == word.length()) return true;
-11        if (low == word.length()) return true;
-12        if (upp == 1 && Character.isUpperCase(word.charAt(0))) return true;
-13        return false;
-14    }
-15}
+2    public String reverseStr(String s, int k) {
+3        StringBuilder sb = new StringBuilder();
+4        for(int i = 0; i < s.length(); i++){
+5            sb.append(s.charAt(i));
+6        }
+7    
+8        for(int i = 0; i < s.length(); i += 2*k){
+9            int start = i;
+10            int end = Math.min(i+k-1, s.length()-1);
+11            while(start <= end){
+12                char temp = sb.charAt(start);
+13                sb.setCharAt(start, sb.charAt(end));
+14                sb.setCharAt(end, temp);
+15                start++;
+16                end--;
+17            }
+18        }
+19        String str = sb.toString();
+20        return str;
+21    }
+22}
