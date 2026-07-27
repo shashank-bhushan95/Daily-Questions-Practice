a2f4bd23a11ebda3913712c34d32e702d87fdf36
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-27 14:11:23 +0530

    File updated at 7/27/2026, 2:11:23 PM

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,11 +1,10 @@
-// Last updated: 7/27/2026, 2:10:06 PM
+// Last updated: 7/27/2026, 2:11:23 PM
 1class Solution {
 2    public int distributeCandies(int[] candyType) {
-3        Arrays.sort(candyType);
-4        int unique = 1;
-5        for(int i = 0; i < candyType.length-1; i++){
-6            if(candyType[i] != candyType[i+1]) unique++;
-7        }
-8        return Math.min(unique, candyType.length/2);
-9    }
-10}
+3        HashSet<Integer> arr = new HashSet<>();
+4        for(int num : candyType){
+5            arr.add(num);
+6        }
+7        return Math.min(arr.size(), candyType.length / 2);
+8    }
+9}
