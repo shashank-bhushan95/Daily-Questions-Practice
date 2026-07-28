f66bad0e665a7a22c04094fe6f83fa8363b1f8ea
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-07-28 16:11:41 +0530

    File updated at 7/28/2026, 4:11:40 PM

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,30 +1,18 @@
-// Last updated: 7/27/2026, 4:47:52 PM
+// Last updated: 7/28/2026, 4:11:40 PM
 1class Solution {
-2    public int calPoints(String[] operations) {
-3        ArrayList<Integer> arr = new ArrayList<>();
-4        for(int i = 0; i < operations.length; i++){
-5            String curr = operations[i];
-6            if(curr.equals("C") && !arr.isEmpty()){
-7                arr.remove(arr.size()-1);
-8            }
-9            else if(curr.equals("D") && !arr.isEmpty()){
-10                int num = (int) arr.get(arr.size()-1);
-11                arr.add(num*2);
-12            }
-13            else if(curr.equals("+") && arr.size() > 1){
-14                int num1 = (int) arr.get(arr.size()-1);
-15                int num2 = (int) arr.get(arr.size()-2);
-16                arr.add(num1+num2);
-17            }
-18            else{
-19                arr.add(Integer.parseInt(curr));
-20            }
-21        }
-22        int ans = 0;
-23        for(int num : arr){
-24            //num = (int)num;
-25            ans += num;
-26        }
-27        return ans;
-28    }
-29}
+2    public int numJewelsInStones(String jewels, String stones) {
+3        HashSet<Character> set = new HashSet<>();
+4        for(int i = 0; i < jewels.length(); i++){
+5            char ch = jewels.charAt(i);
+6            set.add(ch);
+7        }
+8        int ans = 0;
+9        for(int i = 0; i < stones.length(); i++){
+10            char ch = stones.charAt(i);
+11            if (set.contains(ch)) {
+12                ans++;
+13            }
+14        }
+15        return ans;
+16    }
+17}
