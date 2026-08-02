3440f95bcbca2a277b1dea621e83c32679931c3f
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-08-02 19:30:46 +0530

    File updated at 8/2/2026, 7:30:44 PM

--- 896-MonotonicArray/896-MonotonicArray.java
+++ 896-MonotonicArray/896-MonotonicArray.java
@@ -1,14 +1,12 @@
-// Last updated: 8/2/2026, 7:22:47 PM
+// Last updated: 8/2/2026, 7:30:44 PM
 1class Solution {
 2    public boolean isMonotonic(int[] nums) {
-3        int[] sort = nums.clone();
-4        Arrays.sort(sort);
-5        boolean asc = true;
-6        boolean des = true;
-7        for(int i = 0; i < nums.length; i++){
-8            if(nums[i] != sort[i]) asc = false;
-9            if(nums[i] != sort[nums.length-i-1]) des = false;
-10        }
-11        return asc || des;
-12    }
-13}
+3        boolean asc = true;
+4        boolean des = true;
+5        for(int i = 0; i < nums.length-1; i++){
+6            if(nums[i] > nums[i+1]) asc = false;
+7            if(nums[i] < nums[i+1]) des = false;
+8        }
+9        return asc || des;
+10    }
+11}
