a7871d742c9070e9a4aeeb273797dcab33265d87
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-08-04 23:53:49 +0530

    File updated at 8/4/2026, 11:53:48 PM

--- 925-LongPressedName/925-LongPressedName.java
+++ 925-LongPressedName/925-LongPressedName.java
@@ -1,31 +1,14 @@
-// Last updated: 8/4/2026, 5:00:56 PM
+// Last updated: 8/4/2026, 11:53:48 PM
 1class Solution {
-2    public int numUniqueEmails(String[] emails) {
-3        HashSet<String> ans = new HashSet<>();
-4        for(int i = 0; i < emails.length; i++){
-5            String curr = emails[i];
-6            //if(curr.charAt(0) == '+')
-7            StringBuilder sb = new StringBuilder();
-8            int j = 0;
-9            while(curr.charAt(j) != '@'){
-10                char ch = curr.charAt(j);
-11                if(ch == '.') j++;
-12                else if(ch == '+'){
-13                    while(curr.charAt(j) != '@') {
-14                        j++;
-15                    }
-16                }
-17                else{
-18                    sb.append(ch);
-19                    j++;
-20                }
-21            }
-22            while (j < curr.length()) {
-23                sb.append(curr.charAt(j));
-24                j++;
-25            }
-26            ans.add(sb.toString());
-27        }
-28        return ans.size();
-29    }
-30}
+2    public boolean validMountainArray(int[] arr) {
+3        int index = 0;
+4        while(index < arr.length-1 && arr[index] < arr[index+1]){
+5            index++;
+6        }
+7        if(index == arr.length-1 || index == 0) return false;
+8        while(index < arr.length-1 && arr[index] > arr[index+1]){
+9            index++;
+10        }
+11        return index == arr.length-1;
+12    }
+13}
