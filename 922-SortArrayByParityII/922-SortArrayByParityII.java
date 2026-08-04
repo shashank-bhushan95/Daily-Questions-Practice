4e84b0cfc7c006f0b2b1b9f6c0608abd42272b43
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-08-04 14:37:45 +0530

    File updated at 8/4/2026, 2:37:45 PM

--- 922-SortArrayByParityII/922-SortArrayByParityII.java
+++ 922-SortArrayByParityII/922-SortArrayByParityII.java
@@ -1,4 +1,4 @@
-// Last updated: 8/4/2026, 2:36:55 PM
+// Last updated: 8/4/2026, 2:37:45 PM
 1class Solution {
 2    public int[] sortArrayByParityII(int[] nums) {
 3        int even = 0;
@@ -6,14 +6,15 @@
 5        while(even < nums.length && odd < nums.length){
 6            if(nums[even] % 2 == 0) even += 2;
 7            else if(nums[odd] % 2 != 0) odd += 2;
-8            else if(nums[even] % 2 != 0 && nums[odd] % 2 == 0){
-9                int temp = nums[even];
-10                nums[even] = nums[odd];
-11                nums[odd] = temp;
-12                even += 2;
-13                odd += 2;
-14            }
-15        }
-16        return nums;
-17    }
-18}
+8            else{
+9            //(nums[even] % 2 != 0 && nums[odd] % 2 == 0){
+10                int temp = nums[even];
+11                nums[even] = nums[odd];
+12                nums[odd] = temp;
+13                even += 2;
+14                odd += 2;
+15            }
+16        }
+17        return nums;
+18    }
+19}
