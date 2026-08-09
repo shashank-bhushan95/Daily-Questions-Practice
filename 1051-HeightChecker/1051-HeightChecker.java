de8d569434f2e5222ff1a2a5db4af7d6a391516f
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-08-09 15:39:46 +0530

    File updated at 8/9/2026, 3:39:45 PM

--- 1051-HeightChecker/1051-HeightChecker.java
+++ 1051-HeightChecker/1051-HeightChecker.java
@@ -1,22 +1,24 @@
-// Last updated: 8/9/2026, 2:52:33 PM
+// Last updated: 8/9/2026, 3:39:45 PM
 1class Solution {
-2    public int heightChecker(int[] heights) {
-3        int[] freq = new int[101];
-4        for (int h : heights) {
-5            freq[h]++;
-6        }
-7
-8        int ans = 0;
-9        int index = 0;
-10        for (int h = 1; h <= 100; h++) {
-11            while (freq[h] > 0) {
-12                if (heights[index] != h) {
-13                    ans++;
-14                }
-15                index++;
-16                freq[h]--;
+2    public void duplicateZeros(int[] arr) {
+3        int[] ans = new int[arr.length];
+4        int index = 0;
+5        for(int i = 0; i < arr.length && index < arr.length; i++){
+6            if(arr[i] == 0){
+7                ans[index] = 0;
+8                index++;
+9                if(index < arr.length){
+10                    ans[index] = 0;
+11                    index++;
+12                }
+13            }
+14            else{
+15                ans[index] = arr[i];
+16                index++;
 17            }
 18        }
-19        return ans;
-20    }
-21}
+19        for(int i = 0; i < arr.length; i++){
+20            arr[i] = ans[i];
+21        }
+22    }
+23}
