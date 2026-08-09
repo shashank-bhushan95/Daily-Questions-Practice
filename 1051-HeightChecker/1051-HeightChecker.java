5265acaf50081369c585151061800052eed7ec57
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-08-09 14:52:34 +0530

    File updated at 8/9/2026, 2:52:33 PM

--- 1051-HeightChecker/1051-HeightChecker.java
+++ 1051-HeightChecker/1051-HeightChecker.java
@@ -1,13 +1,22 @@
-// Last updated: 8/9/2026, 2:51:55 PM
+// Last updated: 8/9/2026, 2:52:33 PM
 1class Solution {
 2    public int heightChecker(int[] heights) {
-3        int[] copy = heights.clone();
-4        Arrays.sort(heights);
-5        int ans = 0;
-6        for(int i = 0; i < heights.length; i++){
-7            if(copy[i] != heights[i]) ans++;
-8        }
-9        return ans;
-10    }
-11
-12}
+3        int[] freq = new int[101];
+4        for (int h : heights) {
+5            freq[h]++;
+6        }
+7
+8        int ans = 0;
+9        int index = 0;
+10        for (int h = 1; h <= 100; h++) {
+11            while (freq[h] > 0) {
+12                if (heights[index] != h) {
+13                    ans++;
+14                }
+15                index++;
+16                freq[h]--;
+17            }
+18        }
+19        return ans;
+20    }
+21}
